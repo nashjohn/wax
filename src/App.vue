86e865ad17789529ceb0a5ca040aa27455fcdd05
nashthai <nashjohn@protonmail.com>
2020-06-24 12:22:45 -0400

    newer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -15,7 +11,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height:100vh;
+  width:100vw;
 }
-
-
+body {
+  padding:0px;
+  margin:0px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
- <h1>Waxed</h1>
+  <div class="homer"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-
 </script>
+
+<style scoped>
+div.homer {
+  width: 100vw;
+  height: 100wh;
+  background-color: pink;
+  display: block;
+}
+</style>

828670733c648c5696323de119cbaae6a55049a6
nashthai <nashjohn@protonmail.com>
2020-06-28 23:22:10 -0400

    intro

---
+++ src/components/BibleVerse.vue
@@ -0,0 +1,79 @@
+<template>
+  <transition
+    name="intro"
+    appear
+    appear-active-class="intro-enter-active"
+    appear-to-class="the-end"
+    appear-class="the-show"
+  >
+    <div class="verse">
+      <p class="paul">
+        “Then Paul and Barnabas <span class="waxed"> waxed bold,</span> and
+        said, It was necessary that the word of God should first have been
+        spoken to you: but seeing ye put it from you, and judge yourselves
+        unworthy of everlasting life,”
+      </p>
+    </div>
+  </transition>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped lang="scss">
+.verse {
+  background-color: black;
+  color: white;
+  height: 100vh;
+  width: 100wh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+}
+.waxed {
+  color: yellow;
+}
+.paul {
+  width: 50%;
+  display: block;
+  align-content: middle;
+  line-height: 1.5em;
+  font-size: 2.35em;
+  letter-spacing: 0.05em;
+  font-weight: 10;
+  text-align: left;
+  font-family: "Alegreya Sans", sans-serif;
+  z-index: 100;
+}
+.paul {
+  animation: simon 12s fowards;
+}
+@keyframes simon {
+  0% {
+    opacity: 1;
+  }
+  90% {
+    opacity: 1;
+  }
+  95% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+.verse {
+  animation: in 5s forwards;
+}
+@keyframes in {
+  0% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0;
+    display: none;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="homer"></div>
+  <div class="homer">
+    <BibleVerse></BibleVerse>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import BibleVerse from "@/components/BibleVerse.vue";
+export default {
+  name: "Home",
+  components: {
+    BibleVerse
+  }
+};
 </script>
 
 <style scoped>
 .homer {
   width: 100vw;
   height: 100%;
-  background-color: pink;
   display: block;
-  background-image: url("/images/home.jpg");
+  background-image: url("/images/today.jpg");
   background-size: cover;
 }
 </style>
